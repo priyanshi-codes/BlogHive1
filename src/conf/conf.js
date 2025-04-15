7f2afe6437d1146b82270b74cc6ebcdb6d0c4753
Priyanshi-codes <gupta28priyanshi@gmail.com>
2025-04-15 19:05:12 +0530

    Enhance TinyMCE integration, update Appwrite configuration, and improve component structure
    - Integrated TinyMCE editor in index.html for rich text editing.
    - Updated package-lock.json for caniuse-lite version.
    - Refactored AuthService methods for better error handling and consistency.
    - Improved configuration management in conf.js and config.js.
    - Enhanced UI components (Button, Input, Select) for better accessibility and usability.
    - Updated PostForm to handle image uploads and post submissions more effectively.
    - Refined AllPosts and Home pages for improved layout and responsiveness.

---
+++ src/conf/conf.js
@@ -0,0 +1,11 @@
+const conf={
+    appwriteUrl: String(import.meta.env.VITE_APPWRITE_URL),
+    appwriteProject: String(import.meta.env.VITE_APPWRITE_PROJECT_ID),
+    appwriteKey: String(import.meta.env.VITE_APPWRITE_KEY_ID)   ,
+    appwriteDatabase: String(import.meta.env.VITE_APPWRITE_DATABASE_ID),
+    appwriteCollection: String(import.meta.env.VITE_APPWRITE_COLLECTION_DATABASE_ID),
+    appwriteBucket: String(import.meta.env.VITE_APPWRITE_BUCKET_ID),
+
+}
+
+export default conf
